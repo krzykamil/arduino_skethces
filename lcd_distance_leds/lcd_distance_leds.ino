8ea112f5ba7fd67051161ab37336624f9c863440
krzykamil <krzysiekkamilpiotrek@gmail.com>
2021-01-08 18:37:24 +0100

    add leds to distance monitor

--- lcd_distance_leds/lcd_distance_leds.ino
+++ lcd_distance_leds/lcd_distance_leds.ino
@@ -2,6 +2,7 @@
   Devices used:
   LCD Display: LCM 1602 IIC V1
   Distance Ultrasonic Measurment: HC-SR04
+  Two LEDs with resistors
 */
 
 #include <Wire.h> 
@@ -12,39 +13,42 @@
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 4 lines and 20 chars
 
-int led = 6; // the pin the LED is connected to
+int ledGreen = 6; // the pin the LED is connected to
+int ledRed = 7; // the pin the LED is connected to
+long distance;
 
-void setup()
-{
+void setup() {
   pinMode(trigPin, OUTPUT); //Pin, with trig as exit
   pinMode(echoPin, INPUT); //pin with echo as enter point
   lcd.init(); // initialize the lcd.
   lcd.backlight();
   lcd.setCursor(3,0);
-  lcd.print("STAY AWAY!");
-  pinMode(led, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
 }
 
-void loop()
-{
-  lcd.setCursor(2,1);
-  lcd.print("Something is");
-  lcd.setCursor(10,2);
+void loop() {
+  lcd.setCursor(0,1);
+  lcd.print("Distance(CM): ");
+  lcd.setCursor(15,1);
   lcd.print(measure_distance());
   lcd.setCursor(2,3);
-  lcd.print("cms away!");
-  digitalWrite(led, HIGH);
+  if (distance < 50) {
+    digitalWrite(ledGreen, LOW);
+    digitalWrite(ledRed, HIGH);
+    lcd.print("I NEED AN ADULT!");
+  } else {
+    digitalWrite(ledRed, LOW);
+    digitalWrite(ledGreen, HIGH);
+    lcd.print("This is alright");
+  }
   delay(5000);
-  digitalWrite(led, LOW);
-  delay(2000);
   lcd.clear();
 }
 
-//this method relies on the impuls devise documentation https://www.mpja.com/download/hc-sr04_ultrasonic_module_user_guidejohn.pdf
 int measure_distance ()
 {
-  long time, distance;
-  
+  long time;
   digitalWrite(trigPin, LOW);  //set low state for 2 uS - initilize impuls
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);  //set high state for 10 uS - initilize impuls
